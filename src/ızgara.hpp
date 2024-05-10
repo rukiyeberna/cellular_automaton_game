383240f905121ad6b915b68cb792db8c01d9aca4
rukiyeberna <rb.turan@hotmail.com>
2024-05-10 23:25:12 +0300

    create .hpp

---
+++ src/ızgara.hpp
@@ -0,0 +1,12 @@
+/* 
+.hpp
+Bu bir header dosyasidir
+classlari burda yazariz
+.cpp 
+Burada implementation dosyasidir
+header file da tanimlanan fonklar burda implemente edilir
+
+*****
+amac abstraction of code ---> oop
+*/
+
