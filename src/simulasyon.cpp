aae42f6903f4d8033714ad406f788934087d7601
rukiyeberna <rb.turan@hotmail.com>
2024-06-16 13:34:27 +0300

    simulasyon class v.1

---
+++ src/simulasyon.cpp
@@ -0,0 +1,12 @@
+#include "simulasyon.hpp"
+
+/*cizim metodunun implementasyonu*/
+void Simulasyon::Cizim()
+{
+    izgara.Cizim();
+}
+
+void Simulasyon::DegerEkle(int satir, int sutun, int deger)
+{
+    izgara.DegerEkle(satir,sutun,deger);
+}
--- src/simulasyon.hpp
+++ src/simulasyon.hpp
@@ -6,5 +6,17 @@
 class Simulasyon 
 {
     public:
+        /*constructor ekleyelim*/
+        Simulasyon(int genislik, int yukseklik, int hucreBoyutu)
+        /*Initializer list*/
+        : izgara(genislik,yukseklik,hucreBoyutu) 
+        {
+
+        };
+        void Cizim();
+        void DegerEkle(int satir, int sutun, int deger);
     private:
-};
+        /*attribute ekleyelim*/
+        Izgara izgara;
+
+};      
